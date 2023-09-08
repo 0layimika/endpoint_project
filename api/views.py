27eea947f33d16f29555e9dbf66dd1875ae9f127
0layimika <110624406+0layimika@users.noreply.github.com>
2023-09-08 13:06:03 +0100

    made necessary changes

--- api/views.py
+++ api/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
-from datetime import datetime
+from datetime import datetime, timezone
 from django.http import JsonResponse
 # Create your views here.
 def endpoint(request):
  slack_name = request.GET.get('slack_name', 'Olayimika Kayode')
  track = request.GET.get('track', 'Backend')
- current_date=datetime.now()
- current_time=current_date.strftime('%H:%M:%S')
+ current_date=datetime.now(timezone.utc)
  data = {'message':'This is my api',
-         "Slack_name":slack_name,
-         "Current_day":f"{current_date.strftime('%A')}",
-         "utc_time":f"{current_time}",
+         "slack_name":slack_name,
+         "current_day":f"{current_date.strftime('%A')}",
+         "utc_time":f"{current_date.isoformat()}",
          "track":track,
-         "gitbub_file_url":"https://github.com/0layimika/endpoint_project/tree/main/endpoint",
-         "github_repo":"https://github.com/0layimika/endpoint_project",
+         "github_file_url":"https://github.com/0layimika/endpoint_project/tree/main/endpoint",
+         "github_repo_url":"https://github.com/0layimika/endpoint_project",
          "status_code":200}
  return JsonResponse(data)
